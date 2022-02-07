4c0c30a49c75a93f14c4676e94b04d23c93e0b53
monxchrome <98259661+monxchrome@users.noreply.github.com>
2022-02-07 13:10:11 +0200

    updates done

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('blogs/', views.index, name='index'),
-    path('', views.welcome, name='index'),
-    path('<int:pk>/', views.one_post, name='one_post'),
+    path('', views.index, name='index'),
+    path('updates/<int:pk>/', views.one_post, name='one_post'),
+    path('updates/', views.show_gallery, name='updates'),
+    path('contacts/', views.contacts, name='contacts')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,24 @@
 import datetime
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from . models import Blog
 
-def index(request):
-    blogs = Blog.objects.filter(is_published=True)
-    context = {
-        "blogs": blogs
-    }
+def index(request, **kwargs):
+    blogs = Blog.objects.filter(is_published=True).order_by('-id')[0:3]
+    q = request.GET.get('q')
+    if q:
+        blogs = Blog.objects.filter(is_published=True).filter(title__icontains=q)
+    context = {'blogs': blogs, 'q': q}
     return render(request, 'index.html', context=context)
-
-def welcome(request):
-    return render(request, 'welcome.html',)
 
 def one_post(request, pk):
     post = get_object_or_404(Blog, pk=pk)
-    context = {
-        "blogs": blogs
-    }
-    return render (request, 'one_post', context=context)
+    return render(request, 'one_post.html', context={'post': post})
+
+def show_gallery(request):
+    blogs = Blog.objects.all().order_by('-id')
+    return render(request, 'gallery.html', context={'blogs':blogs})
+
+def contacts(request):
+    return render(request, 'contacts.html')
